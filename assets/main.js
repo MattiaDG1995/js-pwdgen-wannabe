e3ac4bb73c0caca9c911a1a61e3a5e0bfcb181bb
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-10-18 14:30:13 +0200

    fix css

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,10 @@
-console.log('ciao');
+let nome = prompt('scrivi il tuo nome:');
+
+let cognome = prompt('scrivi il tuo cognome:');
+
+let colore = prompt('scrivi il tuo colore preferito:');
+
+let numero = prompt('scrivi il tuo numero preferito:');
+
+document.getElementById('password').innerHTML = `La tua password insicurissima è: ${nome}${cognome}${colore}${numero}`;
+
